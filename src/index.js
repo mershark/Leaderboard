7740e6375e5d4342c1f231d5ae840887b6080b2b
mershark <mersharktakyi@gmail.com>
2023-07-27 14:17:29 +0000

    Correct linter errors

--- src/index.js
+++ src/index.js
@@ -4,22 +4,44 @@ import displayScores from './modules/scorelist.js';
 
 // Event listener for the "Refresh" button
 const refreshBtn = document.querySelector('.refresh-btn');
-refreshBtn.addEventListener('click', displayScores);
+refreshBtn.addEventListener('click', async () => {
+  try {
+    const scores = await getScores(); // Fetch scores from the API
+    displayScores(scores); // Display the fetched scores
+  } catch (error) {
+    // Handle errors here if needed for debugging
+  }
+});
 
 // Event listener for the "Add Score" form submission
 const scoreForm = document.getElementById('score-form');
-scoreForm.addEventListener('submit', (event) => {
+scoreForm.addEventListener('submit', async (event) => {
   event.preventDefault();
   const playerName = document.getElementById('player-name').value;
   const scoreValue = parseInt(document.getElementById('score').value, 10);
 
-  if (playerName && !isNaN(scoreValue)) {
-    addScore(playerName, scoreValue);
-    displayScores(); // Refresh the scores after adding a new score
-    scoreForm.reset(); // Clear the form fields after submission
+  if (playerName && !Number.isNaN(scoreValue)) {
+    try {
+      await addScore(playerName, scoreValue); // Add the score to the API
+      const scores = await getScores();
+      displayScores(scores);
+      scoreForm.reset();
+    } catch (error) {
+      // Handle errors here if needed
+    }
   } else {
     // alert('Please enter a valid name and score.');
   }
 });
 
-window.addEventListener('load', displayScores);
+// Function to initially fetch and display scores on page load
+const initializeScores = async () => {
+  try {
+    const scores = await getScores();
+    displayScores(scores);
+  } catch (error) {
+    // Handle errors here if needed
+  }
+};
+
+window.addEventListener('load', initializeScores);
